f1b8ea642ab89f8f0de54bb0609986129feb8407
Hayden Metsky <hmetsky@gmail.com>
2019-05-22 14:59:56 -0400

    Plot more subsets of data and be more clear about naming

--- plot_data_distribution.R
+++ plot_data_distribution.R
@@ -18,19 +18,29 @@ names(all.data) <- gsub("_", ".", names(all.data))
 
 # Extract the subset of data points with perfect match and
 # canonical NGG PAM
-canonical.match <- subset(all.data, category == "PAM" & grepl(".GG", annotation))
+guide.match.and.good.pam <- subset(all.data, category == "PAM" & grepl(".GG", annotation))
 
 # Extract subset of data points with perfect match but
 # not the canonical NGG PAM
-wrong.pam <- subset(all.data, category == "PAM" & !grepl(".GG", annotation))
+guide.match.and.wrong.pam <- subset(all.data, category == "PAM" & !grepl(".GG", annotation))
 
-# Extract subset of data points with mismatch to target
-mismatch <- subset(all.data, category == "Mismatch")
+# Extract subset of data points with perfect match and any PAM
+# (this is the union of guide.match.and.good.pam and guide.match.and.wrong.pam)
+guide.match <- subset(all.data, category == "PAM")
+
+# Extract subset of data points with mismatch to target and
+# canonical NGG PAM; for ones where category=="Mismatch", the annotation
+# does not give the PAM (unlike when category=="PAM") so we have
+# to check the target sequence context
+guide.mismatch.and.good.pam <- subset(all.data, category == "Mismatch" & grepl(".GG.{17}", guide.wt.context.3))
 
 # Melt all data and the different subsets into a single
 # data frame
-df <- melt(list(all=all.data, canonical.match=canonical.match,
-                wrong.pam=wrong.pam, mismatch=mismatch),
+df <- melt(list(all=all.data,
+                guide.match.and.good.pam=guide.match.and.good.pam,
+                guide.match.and.wrong.pam=guide.match.and.wrong.pam,
+                guide.match=guide.match,
+                guide.mismatch.and.good.pam=guide.mismatch.and.good.pam),
            id.vars=names(all.data))
 names(df)[names(df) == "L1"] <- "dataset"
 
@@ -38,7 +48,7 @@ names(df)[names(df) == "L1"] <- "dataset"
 # In particular, show density of the output variable (day21.minus.etp)
 p <- ggplot(df, aes(x=day21.minus.etp, fill=dataset, color=dataset))
 # Use position='identity' to overlay plots
-p <- p + geom_density(alpha=0.25, position='identity')
+p <- p + geom_density(alpha=0.1, position='identity')
 
 p <- p + ggsave(OUT.PDF, width=8, height=8, useDingbats=FALSE)
 
