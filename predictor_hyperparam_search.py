871bdd2b94baddab1ca746fccdc7acd8330271c7
Hayden Metsky <hmetsky@gmail.com>
2019-06-19 12:24:23 -0400

    Add option to perform random search for hyperparameters

--- predictor_hyperparam_search.py
+++ predictor_hyperparam_search.py
@@ -1,8 +1,12 @@
 """Search over hyperparameters for the predictor.
+
+The grid search here is similar to scikit-learn's GridSearchCV, and the
+random search is similar to its RandomizedSearchCV.
 """
 
 import argparse
 import itertools
+import random
 
 import parse_data
 import predictor
@@ -56,11 +60,10 @@ def hyperparam_grid():
         point in the grid)
     """
     # Map each parameter to a list of possible values
-    """
     grid = {
-            'conv_num_filters': [5, 10, 15, 20, 25],
+            'conv_num_filters': [5, 15, 25],
             'conv_filter_width': [[1], [2], [1, 2], [1, 2, 3, 4]],
-            'pool_window_width': [1, 2, 3, 4],
+            'pool_window_width': [1, 2, 4],
             'fully_connected_dim': [[20], [40],
                 [20, 20], [20, 40], [40, 40],
                 [20, 20, 20], [20, 20, 40], [20, 40, 40], [40, 40, 40]],
@@ -69,19 +72,6 @@ def hyperparam_grid():
             'locally_connected_dim': [1, 2, 4],
             'dropout_rate': [0, 0.25, 0.50],
             'l2_factor': [0, 0.0001, 0.001, 0.01, 0.1],
-            'max_num_epochs': [1000]
-    }
-    """
-    grid = {
-            'conv_num_filters': [10, 15],
-            'conv_filter_width': [[2], [1, 2]],
-            'pool_window_width': [2],
-            'fully_connected_dim': [[40]],
-            'pool_strategy': ['max'],
-            'locally_connected_width': [[1]],
-            'locally_connected_dim': [1],
-            'dropout_rate': [0.25],
-            'l2_factor': [0.0001],
             'max_num_epochs': [1000]
     }
     keys_ordered = sorted(grid.keys())
@@ -93,7 +83,72 @@ def hyperparam_grid():
         yield params
 
 
-def search_for_hyperparams(x, y, num_splits=5, loss_out=None):
+def hyperparam_random_dist(num_samples=100):
+    """Construct distribution of hyperparameters.
+
+    Args:
+        num_samples: number of random points to yield from the space
+
+    Iterates:
+        params where each is a dict of possible parameter values (i.e.,
+        a point selected randomly from the space)
+    """
+    # Define distributions
+    def constant(x):
+        def d():
+            return x
+        return d
+    def uniform_discrete(x):
+        def d():
+            return random.choice(x)
+        return d
+    def uniform_int(low=0, high=10):
+        def d():
+            return np.random.randint(low, high)
+        return d
+    def uniform_continuous(low=0.0, high=1.0):
+        def d():
+            return np.random.uniform(low, high)
+        return d
+    def uniform_nested_dist(num_low, num_high, d_inner):
+        # d_inner is a distribution function
+        # v is chosen uniformly in [num_low, num_high), and v
+        # values are sampled from d_inner and returned
+        def d():
+            v = np.random.randint(num_low, num_high)
+            return [d_inner() for _ in range(v)]
+        return d
+    def lognormal(mean=0.0, stdev=1.0):
+        # mean/stdev describe the log of the returned variable
+        def d():
+            return np.random.lognormal(mean, stdev)
+        return d
+
+    # Map each parameter to a distribution of values
+    space = {
+             'conv_num_filters': uniform_int(5, 30),
+             'conv_filter_width': uniform_discrete([[1], [2], [1, 2], [1, 2, 3,
+                 4]]),
+             'pool_window_width': uniform_int(1, 5),
+             'fully_connected_dim': uniform_nested_dist(1, 4,
+                 uniform_int(20, 50)),
+             'pool_strategy': uniform_discrete(['max', 'avg', 'max-and-avg']),
+             'locally_connected_width': uniform_discrete([None,
+                 [1], [2], [1, 2], [1, 2, 3, 4]]),
+             'locally_connected_dim': uniform_int(1, 5),
+             'dropout_rate': uniform_continuous(0, 0.75),
+             'l2_factor': lognormal(-7.0, 4.0),
+             'max_num_epochs': constant(1000)
+    }
+    for i in range(num_samples):
+        params = {}
+        for k in space.keys():
+            k_dist = space[k]
+            params[k] = k_dist()
+        yield params
+
+
+def search_for_hyperparams(x, y, search_type, num_splits=5, loss_out=None):
     """Search for optimal hyperparameters.
 
     This uses hyperparam_grid() to find the grid of hyperparameters over
@@ -106,6 +161,7 @@ def search_for_hyperparams(x, y, num_splits=5, loss_out=None):
     Args:
         x: input data
         y: labels
+        search_type: 'grid' or 'random' search
         num_splits: number of splits of the data to make (i.e., k in k-fold)
         loss_out: if set, an opened file object to write to write the mean
             validation loss for each choice of hyperparameters
@@ -114,9 +170,16 @@ def search_for_hyperparams(x, y, num_splits=5, loss_out=None):
         tuple (params, loss) where params is the optimal choice of
         hyperparameters and loss is the mean validation loss at that choice
     """
+    if search_type == 'grid':
+        params_iter = hyperparam_grid()
+    elif search_type == 'random':
+        params_iter = hyperparam_random_dist()
+    else:
+        raise Exception("Unknown search type '%s'" % search_type)
+
     best_params = None
     best_loss = None
-    for params in hyperparam_grid():
+    for params in params_iter:
         # Compute a mean validation loss at this choice of params
         val_losses = cross_validate(params, x, y, num_splits)
         mean_loss = np.mean(val_losses)
@@ -130,8 +193,8 @@ def search_for_hyperparams(x, y, num_splits=5, loss_out=None):
     return (best_params, best_loss)
 
 
-def nested_cross_validate(x, y, num_outer_splits=5, num_inner_splits=5,
-        loss_out=None):
+def nested_cross_validate(x, y, search_type,
+        num_outer_splits=5, num_inner_splits=5, loss_out=None):
     """Perform nested cross-validation to validate model and search.
 
     This is useful to verify the overall model and model fitting approach
@@ -146,6 +209,7 @@ def nested_cross_validate(x, y, num_outer_splits=5, num_inner_splits=5,
     Args:
         x: input data
         y: labels
+        search_type: 'grid' or 'random' search
         num_outer_splits: number of folds in the outer cross-validation
             procedure
         num_inner_splits: number of folds in the inner cross-validation
@@ -170,7 +234,7 @@ def nested_cross_validate(x, y, num_outer_splits=5, num_inner_splits=5,
 
         # Start a new model for this fold
         best_params, _ = search_for_hyperparams(x_train, y_train,
-                num_splits=num_inner_splits, loss_out=loss_out)
+                search_type, num_splits=num_inner_splits, loss_out=loss_out)
 
         # Compute a loss for this choice of parameters as follows:
         #   (1) train the model on all the training data (in the inner
@@ -192,8 +256,10 @@ def nested_cross_validate(x, y, num_outer_splits=5, num_inner_splits=5,
 
 
 def main(args):
-    # Seed the predictor
+    # Seed the predictor, numpy, and python's random module
     predictor.set_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
 
     # Read the data
     train_split_frac = 1.0 - args.test_split_frac
@@ -223,12 +289,14 @@ def main(args):
     if args.command == 'hyperparam-search':
         # Search for optimal hyperparameters
         if args.params_mean_val_loss_out_tsv:
+            # Use buffering=1 for line buffering (write each line immediately)
             params_mean_val_loss_out_tsv_f = open(
-                    args.params_mean_val_loss_out_tsv, 'w')
+                    args.params_mean_val_loss_out_tsv, 'w', buffering=1)
         else:
             params_mean_val_loss_out_tsv_f = None
 
         params, loss = search_for_hyperparams(x, y,
+                args.search_type,
                 num_splits=args.hyperparam_search_cross_val_num_splits,
                 loss_out=params_mean_val_loss_out_tsv_f)
 
@@ -277,12 +345,14 @@ def main(args):
 
         # Perform nested cross-validation
         if args.params_mean_val_loss_out_tsv:
+            # Use buffering=1 for line buffering (write each line immediately)
             params_mean_val_loss_out_tsv_f = open(
-                    args.params_mean_val_loss_out_tsv, 'w')
+                    args.params_mean_val_loss_out_tsv, 'w', buffering=1)
         else:
             params_mean_val_loss_out_tsv_f = None
 
         fold_results = nested_cross_validate(x, y,
+                args.search_type,
                 num_outer_splits=args.nested_cross_val_outer_num_splits,
                 num_inner_splits=args.hyperparam_search_cross_val_num_splits,
                 loss_out=params_mean_val_loss_out_tsv_f)
@@ -337,6 +407,10 @@ if __name__ == "__main__":
             help=("Number of splits to use in the outer fold for nested "
                   "cross-validation (the inner fold uses "
                   "HYPERPARAM_SEARCH_CROSS_VAL_NUM_SPLITS splits)"))
+    parser.add_argument('--search-type',
+            choices=['grid', 'random'],
+            default='grid',
+            help=("Type of hyperparameter search ('grid' or 'random')"))
     parser.add_argument('--params-mean-val-loss-out-tsv',
             help=("If set, path to out TSV at which to write the mean "
                   "validation loss (across folds) for each choice of "
