80fc49ab12b9cee2c76c0c8730de45d417ec87ac
Hayden Metsky <hmetsky@gmail.com>
2019-09-21 22:38:43 -0400

    Add note about selection of model from hyperparameter search

--- commands.sh
+++ commands.sh
@@ -8,7 +8,11 @@
 python -u predictor_hyperparam_search.py --dataset cas13 --cas13-subset exp-and-pos --cas13-regress-only-on-active --context-nt 10 --command hyperparam-search --hyperparam-search-cross-val-num-splits 5 --search-type random --num-random-samples 500 --params-mean-val-loss-out-tsv out/cas13-hyperparam-search.exp-and-pos.regress-on-active.tsv --save-models models/predictor_exp-and-pos_regress-on-active --seed 1 &> out/cas13-hyperparam-search.exp-and-pos.regress-on-active.out
 
 # Select model 524b9795 from the above, which has the lowest
-# MSE with SEM <0.1 for both MSE and 1_minus_rho
+#   MSE with SEM <0.1 for both MSE and 1_minus_rho
+# Another reason model 524b9795 looks good: in the ranking of
+#   MSE it is ranked second; in the ranking of 1_minus_rho
+#   it is ranked first. If we average the rankings of MSE and
+#   1_minus_rho for all models, model 524b9795 ranks highest.
 
 # Create test results
 python predictor.py --load-model models/predictor_exp-and-pos_regress-on-active/model-524b9795 --dataset cas13 --cas13-subset exp-and-pos --cas13-regress-only-on-active --context-nt 10 --write-test-tsv out/cas13-hyperparam-search.exp-and-pos.regress-on-active.model-524b9795.test.tsv.gz --seed 1 &> out/cas13-hyperparam-search.exp-and-pos.regress-on-active.model-524b9795.test.out
