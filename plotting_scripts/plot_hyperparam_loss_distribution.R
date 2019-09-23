81a5d30036eda54ee0033e904cba027327a99c5a
Hayden Metsky <hmetsky@gmail.com>
2019-09-23 12:15:50 -0400

    Update hyperparameter loss distribution plotting script based on changes to output of hyperparameter search

--- plotting_scripts/plot_hyperparam_loss_distribution.R
+++ plotting_scripts/plot_hyperparam_loss_distribution.R
@@ -7,30 +7,32 @@
 
 
 require(ggplot2)
+require(gridExtra)
 require(reshape2)
 require(stringr)
 
 args <- commandArgs(trailingOnly=TRUE)
-cas <- args[1]  # 'cas9' or 'cas13'
+IN.TSV <- args[1]
+OUT.PDF <- args[2]
 
-IN.RESULTS <- paste0("../out/", cas, "-hyperparam-search.all.tsv")
-OUT.PDF <- paste0("../out/", cas, "-hyperparam-search.all.distributions.pdf")
+# Read hyperparameter results and replace '_' with '.' in column names
+hyperparams <- read.table(IN.TSV, header=TRUE, sep="\t")
+names(hyperparams) <- gsub("_", ".", names(hyperparams))
 
-# Read hyperparameter results
-hyperparams <- read.table(IN.RESULTS, header=FALSE, sep="\t")
-names(hyperparams) <- c("hyperparams", "mean.val.loss")
+# R renames the column '1.minus.rho.mean' to 'X1.minus.rho.mean' when reading; rename it back
+colnames(hyperparams)[colnames(hyperparams) == "X1.minus.rho.mean"] <- "1.minus.rho.mean"
 
 # Add a column giving whether or not a choice of hyperparameters includes
 # a locally connected layer (note the '!' in front of 'grepl')
 hyperparams$has.lc.layer <- !grepl("locally_connected_width: None",
-                                   hyperparams$hyperparams,
+                                   hyperparams$params,
                                    fixed=TRUE)
 
 # Add a column giving the particular locally connected width choice
 # If has.lc.layer is FALSE, the width will not have been able to have been
 # parsed, so set it to None
 lc.width.pattern <- regex("locally_connected_width: (\\[.+\\]),")
-hyperparams$lc.width <- str_match(hyperparams$hyperparams,
+hyperparams$lc.width <- str_match(hyperparams$params,
                                   lc.width.pattern)[,2]
 hyperparams[hyperparams$has.lc.layer == FALSE, ]$lc.width <- "None"
 hyperparams$lc.width <- factor(hyperparams$lc.width)
@@ -39,23 +41,37 @@ hyperparams$lc.width <- factor(hyperparams$lc.width)
 # is no layer, the dimension (a random choice) is meaningless for the model,
 # so set it to 0
 lc.dim.pattern <- regex("locally_connected_dim: (\\d+),")
-hyperparams$lc.dim <- str_match(hyperparams$hyperparams,
+hyperparams$lc.dim <- str_match(hyperparams$params,
                                 lc.dim.pattern)[,2]
 hyperparams[hyperparams$has.lc.layer == FALSE, ]$lc.dim <- 0
 
+# Compute Spearman's rho
+hyperparams$rho.mean <- 1.0 - hyperparams$`1.minus.rho.mean`
+
 # Plot a box plots for each choice of has.lc.layer
-p <- ggplot(hyperparams, aes(has.lc.layer, mean.val.loss))
-p <- p + geom_boxplot(outlier.shape=NA) + geom_jitter(width=0.2)
-
-p <- p + ylab("Mean validation loss (1-AUC)")
-p <- p + xlab("Uses LC layer")
-
-# Make axis labels bigger
-p <- p + theme(axis.text=element_text(size=14),
-               axis.title=element_text(size=18))
+# Produce plot of MSE
+p.mse <- ggplot(hyperparams, aes(x=has.lc.layer, y=mse.mean))
+p.mse <- p.mse + geom_boxplot(outlier.shape=NA) + geom_jitter(width=0.2)
+p.mse <- p.mse + ylim(0, 0.75)   # ignore the outliers >1.0
+p.mse <- p.mse + ylab("Mean validation MSE")
+p.mse <- p.mse + xlab("Uses LC layer")
+p.mse <- p.mse + theme_bw()
+p.mse <- p.mse + theme(axis.text=element_text(size=14),
+                       axis.title=element_text(size=18))
+# Produce plot of rho
+p.rho <- ggplot(hyperparams, aes(x=has.lc.layer, y=rho.mean))
+p.rho <- p.rho + geom_boxplot(outlier.shape=NA) + geom_jitter(width=0.2)
+p.rho <- p.rho + ylab("Mean validation Spearman's rho")
+p.rho <- p.rho + xlab("Uses LC layer")
+p.rho <- p.rho + theme_bw()
+p.rho <- p.rho + theme(axis.text=element_text(size=14),
+                       axis.title=element_text(size=18))
 
 # Save plot to PDF
-p <- p + ggsave(OUT.PDF, width=5, height=6, useDingbats=FALSE)
+g <- arrangeGrob(p.mse,
+                 p.rho,
+                 ncol=1)
+ggsave(OUT.PDF, g, width=8, height=16, useDingbats=FALSE)
 
 # Remove the empty Rplots.pdf created above
 file.remove("Rplots.pdf")
