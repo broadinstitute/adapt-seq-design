001b08feda77bab23f57ecf199f6ee0b8752a8a1
Hayden Metsky <hmetsky@gmail.com>
2021-08-25 09:32:39 -0400

    Make separate plots on only data from Tambe et al. where mismatches hurt activity

--- misc/test-tambe-2018-data/plot_binding_specificity_comparison.R
+++ misc/test-tambe-2018-data/plot_binding_specificity_comparison.R
@@ -17,25 +17,29 @@ args <- commandArgs(trailingOnly=TRUE)
 IN.TSV <- args[1]
 OUT.PDF.SCATTER <- args[2]
 OUT.PDF.QUARTILES <- args[3]
+OUT.PDF.SCATTER.HARM <- args[4]
 
 # Read TSV of results and replace '_' in column names with '.'
 test.results <- read.table(gzfile(IN.TSV), header=TRUE, sep="\t")
 names(test.results) <- gsub("_", ".", names(test.results))
 
-# Randomly sample 1,000 data points per choice of mismatches;
-# there are fewer than 1,000 for 0 and 1 mismatch so this will
+# Randomly sample 150 data points per choice of mismatches;
+# there are fewer than 150 for 0 mismatches so this will
 # use all for those. The reason for this is that there are so
 # many data points with 3 and 4 mismatches -- overwhemling
 # the number with 0, 1, or 2 -- and the plot/metrics may not
 # account for those with fewer mismatches or for the model being
 # able to predict differences in activity across different
-# numbers of mismatches. `min(n(), 1000)` ensures that when
-# there are fewer than 1,000 data points for a choice of
-# mismatches (group), it can still work
+# numbers of mismatches. `min(n(), 150)` ensures that when
+# there are fewer than 150 data points for a choice of
+# mismatches (group), it can still work#
+# 150 because there are 156 total data points with 1 mismatch,
+# (and more than 150 for 2+ mismatches), so this will use almost
+# all of those
 set.seed(1)
 test.results <- test.results %>%
     group_by(number.of.mismatches) %>%
-    sample_n(min(n(), 1000), replace=FALSE)
+    sample_n(min(n(), 150), replace=FALSE)
 
 # Compute metrics comparing the Tambe et al. measurement with the ADAPT model's
 # prediction
@@ -49,11 +53,26 @@ metrics <- function(x, y) {
 test.results.metrics <- metrics(test.results$tambe.value, test.results$adapt.prediction)
 test.results.rho.str <- format(test.results.metrics$rho, digits=3)
 test.results.rho.expr <- as.expression(bquote(rho~"="~.(test.results.rho.str)))
+test.results.r.str <- format(test.results.metrics$r, digits=3)
+test.results.r.expr <- as.expression(bquote(r~"="~.(test.results.r.str)))
 
 print(test.results.metrics$str)
 
 # Since there are only a few choices of number of mismatches, convert it to a factor
 test.results$number.of.mismatches <- factor(test.results$number.of.mismatches)
+
+# Also compute and print metrics for the data where mismatches harm activity
+guide.x <- 'CGCCTGAACCACCAGGCTAT'
+guide.x.wildtype.val <- 0.5478719380654253
+guide.y <- 'CCGCACTATCGGAAGTTCAC'
+guide.y.wildtype.val <- 0.8092551340703578
+test.results.harm <- test.results[(grepl(guide.x, test.results$guide) & test.results$tambe.value <= guide.x.wildtype.val) | (grepl(guide.y, test.results$guide) & test.results$tambe.value <= guide.y.wildtype.val),]
+test.results.harm.metrics <- metrics(test.results.harm$tambe.value, test.results.harm$adapt.prediction)
+print(paste("Subset where mismatches hurt activity:", test.results.harm.metrics$str))
+test.results.harm.rho.str <- format(test.results.harm.metrics$rho, digits=3)
+test.results.harm.rho.expr <- as.expression(bquote(rho~"="~.(test.results.harm.rho.str)))
+test.results.harm.r.str <- format(test.results.harm.metrics$r, digits=3)
+test.results.harm.r.expr <- as.expression(bquote(r~"="~.(test.results.harm.r.str)))
 
 # Compute quartiles
 test.results$tambe.value.quartile <- factor(ntile(test.results$tambe.value, 4))
@@ -67,11 +86,13 @@ p <- ggplot(test.results, aes(x=tambe.value, y=adapt.prediction)) +
         #stat_density_2d(aes(fill=stat(level)), color="#5E5E5E", alpha=0.2, size=0.05, contour=TRUE, n=c(1000, 1000), h=NULL, adjust=1.75) +   # outline around contours; h=c(0.6, 0.6) works too
         #scale_fill_gradient(name="Level", breaks=c(0.2, 0.5), low="#FDF5FF", high="#470E55") +  # customize colors; specify tick labels on legend bar
         xlab("Measured fold-change for binding (Tambe et al. 2018)") + ylab("Model prediction for detection") +
-        scale_color_viridis(discrete=TRUE) + # adjust colors
+        scale_color_viridis(discrete=TRUE, name="Mismatches") + # adjust colors
         theme_pubr() +
         theme(aspect.ratio=1) +  # make plot be square
         annotate(geom="text", label=test.results.rho.expr, # include text with rho value
-                 x=Inf, y=Inf, hjust=1, vjust=1, size=3)
+                 x=Inf, y=Inf, hjust=1, vjust=1, size=3) +
+        annotate(geom="text", label=test.results.r.expr, # include text with r value
+                 x=Inf, y=Inf, hjust=1, vjust=2, size=3)
 ggsave(OUT.PDF.SCATTER, p, width=4.5, height=4.5, useDingbats=FALSE)
 
 # Plot stacked bar for each quartile of ADAPT's predicted value, showing
@@ -88,3 +109,17 @@ p <- ggplot(test.results, aes(x=adapt.prediction.quartile, fill=forcats::fct_rev
               axis.ticks.x=element_blank())
 ggsave(OUT.PDF.QUARTILES, p, width=7, height=4.5, useDingbats=FALSE)
 
+# Plot scatter plot of measured Tambe val. versus predicted value, only for
+# data where the Tambe value decreases against the wildtype (i.e., mismatches
+# harms activity)
+p <- ggplot(test.results.harm, aes(x=tambe.value, y=adapt.prediction)) +
+        geom_point(aes(color=number.of.mismatches), size=1, stroke=0, alpha=0.8) +
+        xlab("Measured fold-change for binding (Tambe et al. 2018)") + ylab("Model prediction for detection") +
+        scale_color_viridis(discrete=TRUE, name="Mismatches") + # adjust colors
+        theme_pubr() +
+        theme(aspect.ratio=1) +  # make plot be square
+        annotate(geom="text", label=test.results.harm.rho.expr, # include text with rho value
+                 x=Inf, y=Inf, hjust=1, vjust=1, size=3) +
+        annotate(geom="text", label=test.results.harm.r.expr, # include text with r value
+                 x=Inf, y=Inf, hjust=1, vjust=2, size=3)
+ggsave(OUT.PDF.SCATTER.HARM, p, width=4.5, height=4.5, useDingbats=FALSE)
--- misc/test-tambe-2018-data/plot_cleavage_rate_comparison.R
+++ misc/test-tambe-2018-data/plot_cleavage_rate_comparison.R
@@ -15,6 +15,7 @@ require(ggstance)
 args <- commandArgs(trailingOnly=TRUE)
 IN.TSV <- args[1]
 OUT.PDF <- args[2]
+OUT.PDF.HARM <- args[3]
 
 # Read TSV of results and replace '_' in column names with '.'
 test.results <- read.table(IN.TSV, header=TRUE, sep="\t")
@@ -32,25 +33,49 @@ metrics <- function(x, y) {
 test.results.metrics <- metrics(test.results$tambe.value, test.results$adapt.prediction)
 test.results.rho.str <- format(test.results.metrics$rho, digits=3)
 test.results.rho.expr <- as.expression(bquote(rho~"="~.(test.results.rho.str)))
+test.results.r.str <- format(test.results.metrics$r, digits=3)
+test.results.r.expr <- as.expression(bquote(r~"="~.(test.results.r.str)))
 
 print(test.results.metrics$str)
+
+# Since there are only a few choices of number of mismatches, convert it to a factor
+test.results$number.of.mismatches <- factor(test.results$number.of.mismatches)
 
 # Also compute and print metrics for the data where mismatches harm activity
 test.results.harm <- test.results[test.results$tambe.value <= 100,]
 test.results.harm.metrics <- metrics(test.results.harm$tambe.value, test.results.harm$adapt.prediction)
 print(paste("Subset where mismatches hurt activity:", test.results.harm.metrics$str))
+test.results.harm.rho.str <- format(test.results.harm.metrics$rho, digits=3)
+test.results.harm.rho.expr <- as.expression(bquote(rho~"="~.(test.results.harm.rho.str)))
+test.results.harm.r.str <- format(test.results.harm.metrics$r, digits=3)
+test.results.harm.r.expr <- as.expression(bquote(r~"="~.(test.results.harm.r.str)))
 
-# Since there are only a few choices of number of mismatches, convert it to a factor
-test.results$number.of.mismatches <- factor(test.results$number.of.mismatches)
-
+# Scatter plot of all results
 p <- ggplot(test.results, aes(x=tambe.value, y=adapt.prediction)) +
         geom_point(aes(color=number.of.mismatches), size=3) +
         xlab("Measured cleavage rate (Tambe et al. 2018)") + ylab("Model prediction") +
         geom_vline(xintercept=100, linetype="dashed") +   # vertical line at normalized (0-mismatch) Tambe et al. value
-        scale_color_viridis(discrete=TRUE) + # adjust colors
+        scale_color_viridis(discrete=TRUE, name="Mismatches") + # adjust colors
         theme_pubr() +
         theme(aspect.ratio=1) +  # make plot be square
         annotate(geom="text", label=test.results.rho.expr, # include text with rho value
-                 x=Inf, y=Inf, hjust=1, vjust=1, size=3)
+                 x=Inf, y=Inf, hjust=1, vjust=1, size=3) +
+        annotate(geom="text", label=test.results.r.expr, # include text with r value
+                 x=Inf, y=Inf, hjust=1, vjust=2, size=3)
 ggsave(OUT.PDF, p, width=4.5, height=4.5, useDingbats=FALSE)
+
+# Scatter plot of only data where the Tambe value decreases against the
+# wildtype (i.e., mismatches harm activity)
+p <- ggplot(test.results.harm, aes(x=tambe.value, y=adapt.prediction)) +
+        geom_point(aes(color=number.of.mismatches), size=3) +
+        xlab("Measured cleavage rate (Tambe et al. 2018)") + ylab("Model prediction") +
+        geom_vline(xintercept=100, linetype="dashed") +   # vertical line at normalized (0-mismatch) Tambe et al. value
+        scale_color_viridis(discrete=TRUE, name="Mismatches") + # adjust colors
+        theme_pubr() +
+        theme(aspect.ratio=1) +  # make plot be square
+        annotate(geom="text", label=test.results.harm.rho.expr, # include text with rho value
+                 x=Inf, y=Inf, hjust=1, vjust=1, size=3) +
+        annotate(geom="text", label=test.results.harm.r.expr, # include text with r value
+                 x=Inf, y=Inf, hjust=1, vjust=2, size=3)
+ggsave(OUT.PDF.HARM, p, width=4.5, height=4.5, useDingbats=FALSE)
 
